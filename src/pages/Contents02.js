1426d411c90eb6f707e9e67e7fda0eaeaca99675
김경찬 <threechany@daum.net>
2023-09-18 15:48:31 +0900

    final09.18

--- src/pages/Contents02.js
+++ src/pages/Contents02.js
@@ -1,36 +1,106 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styles from './css/contents02.module.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 export default function Contents02() {
+
+  const [allContents, setAllContents] = useState([])
+  
+  useEffect(()=>{
+    axios.get('/data/contents.json').then((res)=>(
+      setAllContents(res.data)
+    ))
+  }, [])
+  console.log(allContents)
+
+  // const menulist = [
+  //   {index:0, img:"/images/contents02_img01.png", name:"SB RECYCLING",  title:"[ 연구소 로재시공 및 정비 ]", text:"제품의 재질, 수량,용도, 품질, 납기, 예산 등을 고려 최적의 제작공법을 선정 최단 시일 내 고객이 요구하는 시제품을 도면 SPEC에 맞추어 성형"},
+  //   {index:1, img:"/images/contents02_img02.png", name:"SAND CORE",  title:"[ 22222222222 ]", text:"2222222222222222222"},
+  //   {index:2, img:"/images/contents02_img03.png", name:"Automotive Parts",  title:"[ 33333333333 ]", text:"333333333333333"},
+  //   {index:3, img:"/images/contents02_img04.png", name:"SPECIALTY SB",  title:"[ 444444444444 ]", text:"4444444444444444444"},
+  //   {index:4, img:"/images/contents02_img01.png", name:"COLD ROLLED STEEL SHEETS", title:"[ 555555555555 ]", text:"55555555555555555"},
+  // ]  
+  const menulist = [
+    {index:0, name:"SB RECYCLING", detail:[{img:"/images/contents02_img01.png", title:"[ 연구소 로재시공 및 정비 ]", text:"제품의 재질, 수량,용도, 품질, 납기, 예산 등을 고려 최적의 제작공법을 선정 최단 시일 내 고객이 요구하는 시제품을 도면 SPEC에 맞추어 성형"}]},
+    {index:1, name:"SAND CORE", },
+    {index:2, name:"Automotive Parts"},
+    {index:3, name:"SPECIALTY SB"},
+    {index:4, name:"COLD ROLLED STEEL SHEETS"},
+  ]
+
+
+  const [selectContents, setSelectContents] = useState(menulist[0].name) //내가 고른 컨텐츠메뉴
+
+  const [categoryContents, setCategoryContents] = useState([])  //내가 고른 컨텐츠메뉴의 컨텐츠정보
+  console.log(categoryContents)
+  useEffect(()=>{
+    if(selectContents===menulist[0].name){
+      setCategoryContents(allContents.filter((item)=>(item.category)===selectContents))
+    }else if(selectContents==="SAND CORE"){
+      setCategoryContents(allContents.filter((item)=>(item.category)===selectContents))
+    }else if(selectContents===menulist[2].name){
+      setCategoryContents(allContents.filter((item)=>(item.category)===selectContents))
+    }else if(selectContents===menulist[3].name){
+      setCategoryContents(allContents.filter((item)=>(item.category)===selectContents))
+    }else if(selectContents===menulist[4].name){
+      setCategoryContents(allContents.filter((item)=>(item.category)===selectContents))
+    }
+
+    // if(selectContents===menulist.name){
+    //   setCategoryContents(allContents.filter((item)=>(item.category)===selectContents))
+    // }
+  }, [allContents, selectContents])
+
+
+
   return (
     <div id={styles.contents02_wrap}>
 
         <div id={styles.contents02_img}>
-          
+          {/* <img src='/images/contents02_img01.png'/> */}
+          {
+            categoryContents.map((item)=>(
+              <img src={item.img}></img>
+            ))
+          }
         </div>
 
         <div id={styles.contents02_detail}>
 
           <div id={styles.detail}>
-            <p className={styles.title}>SECOND BATTERY BUSINESS</p>
+            {
+              categoryContents.map((item)=>(
+                <>
+                <p className={styles.title}>{item.title}</p>
+                <p className={styles.sub_title}>{item.subtitle}</p>
+                <p className={styles.text}>{item.text}
+                </p>
+                </>
+              ))
+            }
+            {/* <p className={styles.title} >SECOND BATTERY BUSINESS</p>
+
             <p className={styles.sub_title}>&#91; 연구소 로재시공 및 정비 &#93;</p>
-            <p className={styles.text}>제품의 재질, 수량,용도, 품질, 납기, 예산 등을<br></br>
-                                      고려 최적의 제작공법을 선정<br></br>
-                                      최단 시일 내 고객이 요구하는 시제품을<br></br>
+            <p className={styles.text}>제품의 재질, 수량,용도, 품질, 납기, 예산 등을
+                                      고려 최적의 제작공법을 선정
+                                      최단 시일 내 고객이 요구하는 시제품을
                                       도면 SPEC에 맞추어 성형
-            </p>
+            </p> */}
             <p className={styles.product}><Link to='/'>자세히 보기</Link><span><img src='./images/contents02_arrow.png'></img></span></p>
           </div>
 
         </div>
 
         <ul id={styles.menulist}>
-          <li>SB RECYCLING</li>
-          <li>SAND CORE</li>
-          <li>Automotive Parts</li>
-          <li>SPECIALTY SB</li>
-          <li>COLD ROLLED STEEL SHEETS</li>
+          {
+            menulist.map((item)=>(
+              <li key={item.index}
+              onClick={()=>{setSelectContents(item.name)}}>{item.name}</li>
+            ))
+          }
+
         </ul>
     </div>
   )
 }
+
--- src/pages/Detail_list.js
+++ src/pages/Detail_list.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
 import styles from './css/detail.module.css'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
@@ -9,18 +9,33 @@ import { getProductDetail } from '../api/firebase'
 
 export default function () {
 
-      const [allProducts] = useProducts()
 
       const {productId} = useParams()
 
       const [productItem, setProductItem] = useState([])
 
+      const [clickImg, setClickImg] = useState([]) //대기중인 클릭 서브 이미지
+
+      const [clickIndex, setClickIndex] = useState(0) //대기중인 이미지 클릭 순번
+     
+
+
         useEffect(()=>{
-          setProductItem(allProducts.filter((item)=>(item.id===productId)))
-          // getProductDetail(productId).then((res)=>(
-          //   setProductItem(res)
-          // ))
-        }, [allProducts])
+          getProductDetail(productId).then((res)=>{
+            setProductItem(res)
+
+            setClickImg(Object.values(res.subimg))
+
+          //  setClickImg(Object.values(res.subimg))
+          })
+
+        }, [productId])
+
+
+
+        
+
+
 
   return (
     <div>
@@ -39,15 +54,83 @@ export default function () {
             
             <p className={styles.kind_title}><span><i class="fa-solid fa-bolt-lightning"></i></span>이차전지 종류</p>
     
-            {
-              productItem.map((item)=>(
                 <>
                 <div className={styles.product}>
-                   <p className={styles.product_title}>{item.title}</p>
-                   <p className={styles.product_img}><img src={item.img} alt=''/></p>
-                   <p className={styles.product_text}>{item.text}</p>
+                  
+                    
+                   <>
+                    <p className={styles.product_title}>{productItem.title}</p>
+                    <p className={styles.product_img}><img src={productItem.img} alt=''/></p>
+                   </>
+                  
+                   {/* {
+                    changeImg.map((item)=>{
+                      <p className={styles.product_img}><img src={item.img} alt=''/></p>
+                    })
+                   } */}
+                   <p className={styles.product_text}>{productItem.text}</p>
+                   
+                   <ul className={styles.changeimg}>
+                    {
+                      clickImg.map((item)=>(
+                        <li key={item.index}
+                        onClick={()=>{setProductItem({...productItem, img:item.img})
+                        setClickIndex(item.index)}}>
+                        <img src={item.img} style={item.index===clickIndex ? {border:'solid 2px red'} : {border:'none'}}/></li>
+                      ))
+                    }
+
+                      {/* {
+                        clickImg.map((item)=>(
+                          <li key={item.index} 
+                          onClick={()=>{
+                            setProductItem({...productItem, img:item.img})
+                            setClickIndex(item.index)
+                          }
+                          }><img src={item.img}
+                          
+                          style={item.index===clickIndex ? {border:'solid 3px red'} : {border:'none'}}
+                          />
+                          </li>
+                        ))
+                      } */}
+                   </ul>
                 </div>
-                {
+                      {
+                        productItem.detail && productItem.detail.map((item)=>(
+                          <div className={styles.detail}>
+                            <p className={styles.detail_title}>{item.title}</p>
+                            <p className={styles.detail_text}>{item.text}</p>
+                          </div>
+                        ))
+                      }
+                      {
+                        productItem.detail && productItem.detail.map((item)=>(
+                        <ul className={styles.detail02}>
+                          
+                            <>
+                            <li>
+                              <p className={styles.detail02_title}>{item.title01}</p>
+                              <p className={styles.detail02_img}><img src={item.img01}/></p>
+                              <p className={styles.detail02_text}>{item.text01}</p>
+                            </li>
+                            <li>
+                              <p className={styles.detail02_title02}>{item.title02}</p>
+                              <p className={styles.detail02_img02}><img src={item.img02}/></p>
+                              <p className={styles.detail02_text02}>{item.text02}</p>
+                            </li>
+                            <li>
+                              <p className={styles.detail02_title03}>{item.title03}</p>
+                              <p className={styles.detail02_img03}><img src={item.img03}/></p>
+                              <p className={styles.detail02_text03}>{item.text03}</p>
+                            </li>
+                            </>
+                            
+                        </ul>
+                        ))
+                      }
+                      
+                {/* {
                   productItem.map((item)=>(
                     <div className={styles.detail}>
                       {
@@ -62,8 +145,8 @@ export default function () {
                       }
                     </div>
                   ))
-                }
-                {
+                } */}
+                {/* {
                   productItem.map((item)=>(
                     <ul className={styles.detail02}>
                       {
@@ -89,33 +172,11 @@ export default function () {
                       }
                     </ul>
                   ))
-                }
+                } */}
                 
-                 {/* <div className={styles.detail}>
-                    <p className={styles.detail_title}>양극재란?</p>
-                    <p className={styles.detail_text}>리튬이온베터리 제조 원가의 35% 이상을 차지하는 핵심 소제리튬의 공급원으로 배터리의 용량과 평균 전 압을 결정전지 내 전기에너지를 저장/방출 가능하게 하는 원료 배터리가 저장할 수 있는 에너지 양은 배터리의 용량에 전압을 곱한 것인데, 양극재가 바로 배터리의 성능을 좌우할 ‘용량’과 ‘전압’을 결정하는 핵심 소재리튬이온배터리를 구성하는 네 가지 요소 중 양극재에 더욱 주목해볼 만한 이유 특히나 양극재 시장은 전기차 및 에너지저장시스템 등의 수요가 급성장함에 따라 연평균 33% 증가해 2025년에는 275만 톤 규모로 성장할 전망</p>
-                 </div>
-
-                 <ul className={styles.detail02}>
-                  <li>
-                    <p className={styles.detail02_title}>NCM니켈-코발트-망간</p>
-                    <p className={styles.detail02_img}><img src='/images/nickel.png'/></p>
-                    <p className={styles.detail02_text}>에너지밀도가 높아 주행거리가 길고 배터리 성능이 안정적</p>
-                  </li>
-                  <li>
-                    <p className={styles.detail02_title02}>NCM니켈-코발트-망간</p>
-                    <p className={styles.detail02_img02}><img src='/images/nickel.png'/></p>
-                    <p className={styles.detail02_text02}>에너지밀도가 높아 주행거리가 길고 배터리 성능이 안정적</p>
-                  </li>
-                  <li>
-                    <p className={styles.detail02_title03}>NCM니켈-코발트-망간</p>
-                    <p className={styles.detail02_img03}><img src='/images/nickel.png'/></p>
-                    <p className={styles.detail02_text03}>에너지밀도가 높아 주행거리가 길고 배터리 성능이 안정적</p>
-                  </li>
-                 </ul> */}
                 </>
-              ))
-            }
+              
+            
     
               
                 {/* <div className={styles.dedatil}><img src='/images/Detail.png' alt=''/></div> */}
